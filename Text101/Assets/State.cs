52659e22ee16e18eb778663cac0c96cd92f30ce9
Tyler Zhu <67493075+zetachan17@users.noreply.github.com>
2020-12-29 18:32:20 -0500

    Lesson 23
    Scriptable objects

---
+++ Text101/Assets/State.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+
+public class State : ScriptableObject
+{
+    [TextArea(10, 14)] [SerializeField] string storyText;
+}
