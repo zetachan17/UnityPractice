bdbd580981e645488a1f714fe26a948115b6fc7f
Tyler Zhu <67493075+zetachan17@users.noreply.github.com>
2021-01-10 02:47:52 -0500

    Lesson 53
    Move Object With Mouse

---
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour  
+{
+    [SerializeField] float screenWidthInUnits = 16f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePosition = new Vector2(mousePosInUnits, transform.position.y);
+        transform.position = paddlePosition;
+    }
+}
